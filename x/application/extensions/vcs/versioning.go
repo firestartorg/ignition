e590560ee491194dd36f53f72aeb3ae325ac25dd
Markus Müllner <m.muellner@firestart.com>
2023-12-14 22:09:23 +0100

    feat: add git branch label to build info metric (#12)
    Signed-off-by: Markus Müllner <m.muellner@firestart.com>

--- x/application/extensions/vcs/versioning.go
+++ x/application/extensions/vcs/versioning.go
@@ -1,21 +1,23 @@
 package vcs
 
 import (
+	"runtime/debug"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/rs/zerolog/log"
 	"gitlab.com/firestart/ignition/x/application"
-	"runtime/debug"
 )
 
 var (
+	Branch string = "undefined"
 	// BuildInfo is the gauge metric exposed
 	BuildInfo = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "build_info",
-			Help: "Metric with a constant '1' value labeled by version and goversion from which the cloud gateway receiver was built.",
+			Help: "Metric with a constant '1' value labeled by app name, app version, git branch and goversion from which the application was built.",
 		},
-		[]string{"app_name", "app_version", "go_version"},
+		[]string{"app_name", "app_version", "git_branch", "go_version"},
 	)
 )
 
@@ -30,11 +32,11 @@ func WithBuildInfo(name string) application.Option {
 		revision, ok := findSetting(info.Settings, "vcs.revision")
 		if !ok {
 			// AddHook the following metric
-			BuildInfo.WithLabelValues(name, "dev", info.GoVersion).Set(1)
+			BuildInfo.WithLabelValues(name, "dev", Branch, info.GoVersion).Set(1)
 			return
 		}
 
-		BuildInfo.WithLabelValues(name, revision.Value, info.GoVersion).Set(1)
+		BuildInfo.WithLabelValues(name, revision.Value, Branch, info.GoVersion).Set(1)
 	}
 }
 
